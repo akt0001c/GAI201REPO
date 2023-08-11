3eb3fd4475ab3fc0577131631f6c4bec62e4c79e
akt0001c <110126989+akt0001c@users.noreply.github.com>
2023-08-11 14:04:43 +0530

    update

---
+++ Zomato_Chronicles_usingPython/Dishes.py
@@ -0,0 +1,11 @@
+class Dishes:
+    def __init__(self,id,name,price,available):
+        self.id=id
+        self.name=name
+        self.price=price
+        self.available=available
+
+    def __str__(self):
+        return f"Dish id : {self.id} ,Dish Name : {self.name} , Dish Price : {self.price} , Available :{self.available}"
+        
+        
---
+++ Zomato_Chronicles_usingPython/MenuOperations.py
@@ -0,0 +1,11 @@
+from Dishes import Dishes
+DishMenu={1:Dishes(1,"Palak Panner",100,"Yes"),
+      2:Dishes(2,"dal-Rice",200,"Yes"),
+      3:Dishes(3,"chbati",10,"Yes"),
+      4:Dishes(4,"shahi panner",150,"Yes"),
+      5:Dishes(5,"Malai kophta",50,"Yes") 
+       }
+
+
+
+
